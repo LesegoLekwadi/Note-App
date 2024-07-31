b0980a94d436b3af91051b7c3757c9e0892bbee6
LesegoLekwadi <lesegolekwadi8@gmail.com>
2024-07-31 23:40:57 +0200

    build

--- NoteApp/src/app/app-routing.module.ts
+++ NoteApp/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NoteComponent } from './note/note.component';
+import { NoteDetailsComponent } from './note-details/note-details.component';
+import { NoteListComponent } from './note-list/note-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'note', component:NoteComponent},
+  {path:'details', component:NoteDetailsComponent},
+  {path:'list', component:NoteListComponent},
+  { path: '', redirectTo: 'note', pathMatch: 'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
