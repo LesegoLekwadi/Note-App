1cfc2da690c221c66b3a87ddef2f7ca9924baa60
LesegoLekwadi <lesegolekwadi8@gmail.com>
2024-08-04 12:59:06 +0200

    update

--- Backend/src/main/java/com/Note/Backend/BackendApplication.java
+++ Backend/src/main/java/com/Note/Backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.Note.noteApp;
+package com.Note.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NoteAppApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NoteAppApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- Backend/src/test/java/com/Note/Backend/BackendApplicationTests.java
+++ Backend/src/test/java/com/Note/Backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.Note.noteApp;
+package com.Note.Backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NoteAppApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
